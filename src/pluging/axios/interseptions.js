5b5fef0fb5c0efeb5a12339b6cb55ca0ca7415f1
Степан Кучеренко <s.kucherenko.pirit@yandex.ru>
2024-03-24 16:10:11 +0700

    Settings vuex store, add fetch movies to state

--- src/pluging/axios/interseptions.js
+++ src/pluging/axios/interseptions.js
@@ -1,13 +1,18 @@
 function setParams(config) {
-  const params = params.config || {};
+  const params = config.params || {};
   const appParams = {
-    apiKey: process.env.VUE_APP_API_KEY,
+    apikey: process.env.VUE_APP_API_KEY,
     plot: "full",
   };
   config.params = Object.assign(params, appParams);
   return config;
 }
 
+function responseData(res) {
+  return res.data;
+}
+
 export default function (axios) {
   axios.interceptors.request.use(setParams);
+  axios.interceptors.response.use(responseData);
 }
--- src/store/modules/movies.js
+++ src/store/modules/movies.js
@@ -1,19 +1,49 @@
-import axios from "axios";
+import IDs from "@/store/mock/imdb_top250";
+import axios from "@/pluging/axios";
+import mutations from "../mutations";
+
+const serializedResponse = (movies) =>
+  movies.reduce((acc, movie) => ((acc[movie.imdbID] = movie), acc), {});
+
+const { MOVIES } = mutations;
 
 const moviesStore = {
   namespaced: true,
-  state: {},
-  getters: {},
-  mutations: {},
+  state: {
+    top250IDs: IDs,
+    moviesPerPage: 12,
+    currentPage: 1,
+    movies: {},
+  },
+  getters: {
+    slicesIDs:
+      ({ top250IDs }) =>
+      (from, to) =>
+        top250IDs.slice(from, to),
+    currentPage: ({ currentPage }) => currentPage,
+    moviesPerPage: ({ moviesPerPage }) => moviesPerPage,
+  },
+  mutations: {
+    [MOVIES](state, value) {
+      state.movies = value;
+    },
+  },
   actions: {
-    async fetchMovies(context) {
-      console.log(context);
-      const response = await axios.get("/", {
-        params: {
-          i: "tt0111161",
-        },
-      });
-      console.log(response);
+    async fetchMovies({ getters, commit }) {
+      try {
+        const { currentPage, moviesPerPage, slicesIDs } = getters;
+        const from = currentPage * moviesPerPage - moviesPerPage;
+        const to = currentPage * moviesPerPage;
+        const moviesToFetch = slicesIDs(from, to);
+
+        const requests = moviesToFetch.map((id) => axios.get(`/?i=${id}`));
+        const response = await Promise.all(requests);
+        const movies = serializedResponse(response);
+
+        commit(MOVIES, movies);
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
 };
